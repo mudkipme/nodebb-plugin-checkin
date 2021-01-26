7a108046495f4ccdd9d3db739cf6085ba1de1455
Mudkip <i@mudkip.me>
2021-01-26 16:53:54 +0000

    Compatibility with latest NodeBB & fix #2

--- library.js
+++ library.js
@@ -1,11 +1,10 @@
-const util = require('util');
-const nconf = module.parent.require('nconf');
-const _ = module.parent.require('lodash');
-const User = module.parent.require('./user');
-const db = module.parent.require('./database');
-const Notifications = module.parent.require('./notifications');
-const routeHelpers = module.parent.require('./routes/helpers');
-const controllerHelpers = module.parent.require('./controllers/helpers');
+const nconf = require.main.require('nconf');
+const _ = require.main.require('lodash');
+const User = require.main.require('./src/user');
+const db = require.main.require('./src/database');
+const Notifications = require.main.require('./src/notifications');
+const routeHelpers = require.main.require('./src/routes/helpers');
+const controllerHelpers = require.main.require('./src/controllers/helpers');
 
 const checkinConfig = nconf.get('checkin') || {
     postReward: true,
@@ -27,7 +26,7 @@ const checkinConfig = nconf.get('checkin') || {
 const checkingIn = new Set();
 
 const Checkin = {
-    getNavigation(items, callback) {
+    async getNavigation(items) {
         items.push({
             route: '/checkin',
             title: '[[checkin:check-in]]',
@@ -36,10 +35,10 @@ const Checkin = {
             textClass: 'visible-xs-inline',
             text: '[[checkin:check-in]]'
         });
-        callback(null, items);
+        return items;
     },
 
-    load({ router, middleware }, callback) {
+    async load({ router, middleware }) {
         const render = async function (req, res, next) {
             try {
                 const data = await doCheckin(req.uid);
@@ -58,45 +57,39 @@ const Checkin = {
         };
 
         routeHelpers.setupPageRoute(router, '/checkin', middleware, [middleware.ensureLoggedIn], render);
-        return setImmediate(callback, null);
     },
 
-    async postCreate({ post, data }, callback) {
+    async postCreate({ post, data }) {
         if (!checkinConfig.postReward) {
-            callback(null, { post, data });
-            return;
+            return { post, data };
         }
-        try {
-            const today = dateKey(Date.now());
-            const [checkedIn, checkinPendingReward] = await Promise.all([
-                util.promisify(db.isSortedSetMember)(`checkin-plugin:${today}`, data.uid),
-                util.promisify(User.getUserField)(data.uid, 'checkinPendingReward')
+
+        const today = dateKey(Date.now());
+        const [checkedIn, checkinPendingReward] = await Promise.all([
+            db.isSortedSetMember(`checkin-plugin:${today}`, data.uid),
+            User.getUserField(data.uid, 'checkinPendingReward')
+        ]);
+        if (checkedIn && checkinPendingReward > 0) {
+            await Promise.all([
+                User.incrementUserReputationBy(data.uid, checkinPendingReward),
+                User.setUserField(data.uid, 'checkinPendingReward', 0)
             ]);
-            //TODO: sort "Most reputation" after awarding reputation.
-            if (checkedIn && checkinPendingReward > 0) {
-                await Promise.all([
-                    util.promisify(User.incrementUserFieldBy)(data.uid, 'reputation', checkinPendingReward),
-                    util.promisify(User.setUserField)(data.uid, 'checkinPendingReward', 0)
-                ]);
-                const noti = await util.promisify(Notifications.create)({
-                    bodyShort: `[[checkin:post-message, ${checkinPendingReward}]]`,
-                    bodyLong: `[[checkin:post-message, ${checkinPendingReward}]]`,
-                    nid: 'checkin_' + data.uid,
-                    from: data.uid,
-                    path: '/checkin'
-                });
-                await util.promisify(Notifications.push)(noti, data.uid);
-            }
-            callback(null, { post, data });
-        } catch (err) {
-            callback(err);
+            const noti = await Notifications.create({
+                bodyShort: `[[checkin:post-message, ${checkinPendingReward}]]`,
+                bodyLong: `[[checkin:post-message, ${checkinPendingReward}]]`,
+                nid: 'checkin_' + data.uid,
+                from: data.uid,
+                path: '/checkin'
+            });
+            await Notifications.push(noti, data.uid);
         }
+        return { post, data };
     },
 
-    appendUserFieldsWhitelist(data, callback) {
+    async appendUserFieldsWhitelist(data) {
         data.whitelist.push('checkinContinuousDays');
         data.whitelist.push('checkinPendingReward');
-        return setImmediate(callback, null, data);
+        return data;
     }
 };
 
@@ -108,7 +101,7 @@ async function doCheckin(uid) {
     const now = Date.now();
     const today = dateKey(now);
     const yesterday = dateKey(now - 864e5);
-    const checkedIn = await util.promisify(db.isSortedSetMember)(`checkin-plugin:${today}`, uid);
+    const checkedIn = await db.isSortedSetMember(`checkin-plugin:${today}`, uid);
 
     let reward = 0;
     if (!checkedIn) {
@@ -119,10 +112,10 @@ async function doCheckin(uid) {
         try {
             checkingIn.add(uid);
             const [rank, checkedInYesterday, continuousDays, total] = await Promise.all([
-                util.promisify(db.increment)(`checkin-plugin:rank:${today}`),
-                util.promisify(db.isSortedSetMember)(`checkin-plugin:${yesterday}`, uid),
-                util.promisify(User.getUserField)(uid, 'checkinContinuousDays'),
-                util.promisify(db.sortedSetCard)(`checkin-plugin:user:${uid}`)
+                db.increment(`checkin-plugin:rank:${today}`),
+                db.isSortedSetMember(`checkin-plugin:${yesterday}`, uid),
+                User.getUserField(uid, 'checkinContinuousDays'),
+                db.sortedSetCard(`checkin-plugin:user:${uid}`)
             ]);
 
             const continuousDay = checkedInYesterday ? (parseInt(continuousDays) || 0) + 1 : 1;
@@ -135,15 +128,15 @@ async function doCheckin(uid) {
             }
 
             await Promise.all([
-                util.promisify(db.sortedSetAdd)(`checkin-plugin:${today}`, rank, uid),
-                util.promisify(db.sortedSetAdd)(`checkin-plugin:user:${uid}`, now, today),
-                util.promisify(db.sortedSetAdd)('checkin-plugin:continuous', continuousDay, uid),
-                util.promisify(db.sortedSetAdd)('checkin-plugin:total', total + 1, uid),
-                util.promisify(User.setUserFields)(uid, {
+                db.sortedSetAdd(`checkin-plugin:${today}`, rank, uid),
+                db.sortedSetAdd(`checkin-plugin:user:${uid}`, now, today),
+                db.sortedSetAdd('checkin-plugin:continuous', continuousDay, uid),
+                db.sortedSetAdd('checkin-plugin:total', total + 1, uid),
+                User.setUserFields(uid, {
                     checkinContinuousDays: continuousDay,
                     checkinPendingReward: reward
                 }),
-                util.promisify(User.incrementUserFieldBy)(uid, 'reputation', reward)
+                User.incrementUserReputationBy(uid, reward)
             ]);
             checkingIn.delete(uid);
         }
@@ -154,16 +147,16 @@ async function doCheckin(uid) {
     }
 
     const [total, rank, userData, todayList, continuousList, totalList] = await Promise.all([
-        util.promisify(db.sortedSetCard)(`checkin-plugin:user:${uid}`),
-        util.promisify(db.sortedSetScore)(`checkin-plugin:${today}`, uid),
-        util.promisify(User.getUserFields)(uid, ['checkinContinuousDays', 'checkinPendingReward']),
-        util.promisify(db.getSortedSetRangeWithScores)(`checkin-plugin:${today}`, 0, 9),
-        util.promisify(db.getSortedSetRevRangeWithScores)('checkin-plugin:continuous', 0, 9),
-        util.promisify(db.getSortedSetRevRangeWithScores)('checkin-plugin:total', 0, 9)
+        db.sortedSetCard(`checkin-plugin:user:${uid}`),
+        db.sortedSetScore(`checkin-plugin:${today}`, uid),
+        User.getUserFields(uid, ['checkinContinuousDays', 'checkinPendingReward']),
+        db.getSortedSetRangeWithScores(`checkin-plugin:${today}`, 0, 9),
+        db.getSortedSetRevRangeWithScores('checkin-plugin:continuous', 0, 9),
+        db.getSortedSetRevRangeWithScores('checkin-plugin:total', 0, 9)
     ]);
 
     const uids = _.map(_.concat(todayList, continuousList, totalList), 'value');
-    const users = _.keyBy(await util.promisify(User.getUsersFields)(uids, ['username', 'userslug']), 'uid')
+    const users = _.keyBy(await User.getUsersFields(uids, ['username', 'userslug']), 'uid')
 
     const [todayMembers, continuousMembers, totalMembers] = [todayList, continuousList, totalList]
         .map(list => list.filter(item => users[item.value] !== undefined))
